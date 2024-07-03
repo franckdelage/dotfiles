1aee1e8f3cedaa6ef3bbdf70e9a3a9ed86e412fa
Franck Delage <franck@web82.fr>
2024-07-03 15:42:34 +0200

    upgrade and reinstall

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -472,11 +472,16 @@ lvim.plugins = {
             shade = 5
           },
         },
+        modes = {
+          search = {
+            enabled = true,
+          },
+        },
       })
     end,
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      -- { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "Z",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
