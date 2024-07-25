f8d11408239618e718cea06a8f99b1737b9c61f7
Franck Delage <franck@web82.fr>
2024-07-25 15:47:15 +0200

    small improvements

--- .config/lvim/lua/user/flash.lua
+++ .config/lvim/lua/user/flash.lua
@@ -15,6 +15,11 @@ M.config = function ()
       },
     },
   })
+
+  vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+  vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+  vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+  vim.keymap.set({ "c" }, "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
 end
 
 return M
