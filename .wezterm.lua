76646bd9f9e7115578702ba58ca4420ae48a6420
Franck Delage <franck@web82.fr>
2024-09-17 11:08:59 +0200

    upgrade plugins and change wezterm opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,7 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.83
+config.window_background_opacity = 0.86
 -- config.macos_window_background_blur = 10
 
 local my_catppucin = wezterm.color.get_builtin_schemes()['Catppuccin Frappe']
