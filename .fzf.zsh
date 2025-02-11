df387d97db5f58191c5187b09cc007cab12f2ff3
Franck Delage <franck@web82.fr>
2025-02-11 15:04:13 +0100

    change fzf and bat themes

--- .fzf.zsh
+++ .fzf.zsh
@@ -50,19 +50,18 @@ _fzf_comprun() {
   esac
 }
 
-# --- setup fzf theme ---
-# fg="#CBE0F0"
-# bg="#011628"
-# bg_highlight="#143652"
-# purple="#B388FF"
-# blue="#06BCE4"
-# cyan="#2CF9ED"
+# Catppuccin FZF theme
 
-# export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--color=selected-bg:#51576d \
+--multi"
 
 # ----- Bat (better cat) -----
 
-export BAT_THEME=tokyonight_night
+export BAT_THEME="Catppuccin Frappe"
 
 # Key bindings
 # ------------
