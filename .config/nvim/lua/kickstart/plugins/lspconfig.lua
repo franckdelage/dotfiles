0b55d240581486840901f67d05bf5c28f04a00c6
Franck Delage <franck@web82.fr>
2025-08-11 13:32:52 +0200

    diagnostics under the line in nvim

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -181,21 +181,52 @@ return {
             [vim.diagnostic.severity.HINT] = '󰌶 ',
           },
         } or {},
-        virtual_text = {
-          -- source = 'if_many',
-          source = true,
-          spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
-        },
+        virtual_text = false,
+        virtual_lines = false,
       }
+
+      local ns = vim.api.nvim_create_namespace("cursor_line_diagnostics")
+
+      local function format_diagnostic(diagnostic)
+        local diagnostic_message = {
+          [vim.diagnostic.severity.ERROR] = diagnostic.message,
+          [vim.diagnostic.severity.WARN]  = diagnostic.message,
+          [vim.diagnostic.severity.INFO]  = diagnostic.message,
+          [vim.diagnostic.severity.HINT]  = diagnostic.message,
+        }
+        return diagnostic_message[diagnostic.severity] or diagnostic.message
+      end
+
+      local function show_line_diagnostics()
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        if not vim.api.nvim_buf_is_valid(bufnr)
+          or not vim.api.nvim_buf_is_loaded(bufnr)
+          or vim.bo[bufnr].buftype ~= "" then
+          return
+        end
+
+        local cursor_line = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+        vim.diagnostic.reset(ns, bufnr)
+
+        local diags = vim.diagnostic.get(bufnr, { lnum = cursor_line })
+
+        if #diags > 0 then
+         local formatted_diags = {}
+          for _, d in ipairs(diags) do
+            local copy = vim.deepcopy(d)
+            copy.message = format_diagnostic(copy)
+            table.insert(formatted_diags, copy)
+          end
+
+          vim.diagnostic.show(ns, bufnr, formatted_diags, { virtual_lines = true })
+        end
+      end
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        callback = show_line_diagnostics,
+      })
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
