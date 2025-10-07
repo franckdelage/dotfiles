2e6f82d0f94870212a35b9495736f8e3692fd44d
Franck Delage <franck@web82.fr>
2025-10-07 15:25:34 +0200

    nvim: improve typescript with inlay hints mostly

--- .config/nvim/lua/lsp/servers/typescript.lua
+++ .config/nvim/lua/lsp/servers/typescript.lua
@@ -5,8 +5,54 @@ M.servers = {
   typescript = {
     cmd = { 'typescript-language-server', '--stdio' },
     filetypes = { 'typescript', 'javascript' },
-    root_patterns = { 'angular.json', 'nx.json', 'package.json', 'tsconfig.json' },
+    root_patterns = { 'nx.json', 'angular.json', 'package.json', 'tsconfig.json' },
     name = 'ts_ls',
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = 'non-relative',
+        includePackageJsonAutoImports = 'auto',
+      },
+    },
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+        suggest = {
+          completeFunctionCalls = true,
+        },
+        preferences = {
+          importModuleSpecifierPreference = 'non-relative',
+          includePackageJsonAutoImports = 'auto',
+        },
+      },
+      javascript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+        suggest = {
+          completeFunctionCalls = true,
+        },
+        preferences = {
+          importModuleSpecifierPreference = 'non-relative',
+          includePackageJsonAutoImports = 'auto',
+        },
+      },
+    },
   },
 }
 
