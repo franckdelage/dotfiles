9a9eef252ad8b5db1a71d6cfdb09655e668d17fd
Franck Delage <franck@web82.fr>
2025-10-08 17:01:56 +0200

    sesh: last tweaks and fzf colors

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -47,10 +47,10 @@ _fzf_comprun() {
 }
 
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
---color=selected-bg:#51576d \
+--color=bg+:#2d3f76,bg:#222436,spinner:#c099ff,hl:#ff966c \
+--color=fg:#c8d3f5,header:#ff966c,info:#82aaff,pointer:#c099ff \
+--color=marker:#c099ff,fg+:#c8d3f5,prompt:#82aaff,hl+:#ff966c \
+--color=selected-bg:#2d3f76 \
 --multi"
 
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
