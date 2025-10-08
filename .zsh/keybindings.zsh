82d6a3f8cce11dec99302eec71d25fa873255422
Franck Delage <franck@web82.fr>
2025-10-08 15:00:28 +0200

    sesh: first configuration

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -6,3 +6,20 @@ bindkey '^n' autosuggest-accept
 VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
 VI_MODE_SET_CURSOR=true
 VI_MODE_CURSOR_VISUAL=5
+
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '^G' sesh-sessions
+bindkey -M vicmd '^G' sesh-sessions
+bindkey -M viins '^G' sesh-sessions
