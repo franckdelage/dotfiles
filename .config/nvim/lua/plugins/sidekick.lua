7feaf9f88261760756e86089018d9b86948e79de
Franck Delage <franck@web82.fr>
2025-10-15 08:47:32 +0200

    nvim: install sidekick

---
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,82 @@
+return {
+  "folke/sidekick.nvim",
+  event = { "BufRead", "BufNew" },
+  opts = {
+    -- add any options here
+    cli = {
+      mux = {
+        backend = "tmux",
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "<tab>",
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
+    {
+      "<c-.>",
+      function() require("sidekick.cli").toggle() end,
+      desc = "Sidekick Toggle",
+      mode = { "n", "t", "i", "x" },
+    },
+    {
+      "<leader>ma",
+      function() require("sidekick.cli").toggle() end,
+      desc = "Sidekick Toggle CLI",
+    },
+    {
+      "<leader>ms",
+      function() require("sidekick.cli").select() end,
+      -- Or to select only installed tools:
+      -- require("sidekick.cli").select({ filter = { installed = true } })
+      desc = "Select CLI",
+    },
+    {
+      "<leader>md",
+      function() require("sidekick.cli").close() end,
+      desc = "Detach a CLI Session",
+    },
+    {
+      "<leader>mt",
+      function() require("sidekick.cli").send({ msg = "{this}" }) end,
+      mode = { "x", "n" },
+      desc = "Send This",
+    },
+    {
+      "<leader>mf",
+      function() require("sidekick.cli").send({ msg = "{file}" }) end,
+      desc = "Send File",
+    },
+    {
+      "<leader>mv",
+      function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+      mode = { "x" },
+      desc = "Send Visual Selection",
+    },
+    {
+      "<leader>mp",
+      function() require("sidekick.cli").prompt() end,
+      mode = { "n", "x" },
+      desc = "Sidekick Select Prompt",
+    },
+    {
+      "<leader>mc",
+      function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end,
+      desc = "Sidekick Toggle Copilot",
+    },
+    {
+      "<leader>mo",
+      function() require("sidekick.cli").toggle({ name = "opencode", focus = true }) end,
+      desc = "Sidekick Toggle OpenCode",
+    },
+  },
+}
