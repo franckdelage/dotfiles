90cb44940f070f7daca8de12f43b07ffcb476df5
Franck Delage <franck@web82.fr>
2025-12-22 08:38:08 +0100

    zsh: fix nvm path after setting XDG_CONFIG_HOME

--- .zsh/languages.zsh
+++ .zsh/languages.zsh
@@ -2,7 +2,7 @@ export NVM_COMPLETION=true
 export NVM_SYMLINK_CURRENT="true"
 export NVM_AUTO_USE=true
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 autoload -U add-zsh-hook
