5424e22bff57054d6a6d8469a935be67d63affeb
Franck Delage <franck@web82.fr>
2026-04-07 08:55:49 +0200

    nvim: add ufo and setup folding

---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,55 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
+  event = "BufReadPost",
+  config = function()
+    local handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, {chunkText, hlGroup})
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, {suffix, 'MoreMsg'})
+      return newVirtText
+    end
+
+    require("ufo").setup {
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+      fold_virt_text_handler = handler,
+    }
+  end,
+  keys = {
+    { "zR", function() require("ufo").openAllFolds() end, desc = "Open all folds" },
+    { "zM", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
+    {
+      "K",
+      function()
+        -- Peek fold if cursor is on a closed fold; otherwise fall back to LSP hover.
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then vim.lsp.buf.hover() end
+      end,
+      desc = "Peek fold / LSP hover",
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
