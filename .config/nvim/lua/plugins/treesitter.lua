b42e153a85d97d1c1e3c3197396617bf792dda2b
Franck Delage <franck@web82.fr>
2026-04-08 15:58:21 +0200

    nvim: lazy upgrade and pin treesitter to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
     config = function()
