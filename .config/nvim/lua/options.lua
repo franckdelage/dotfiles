c61ba4e352fe2fa4bbc46ac3064d3fd0cfe09d0f
Franck Delage <franck@web82.fr>
2026-04-23 09:29:44 +0200

    nvim: deactivate swapfile

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -83,6 +83,8 @@ vim.filetype.add {
   },
 }
 
+vim.opt.swapfile = false
+
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 vim.wo.wrap = true
