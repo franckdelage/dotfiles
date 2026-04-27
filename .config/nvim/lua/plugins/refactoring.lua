a95091c691c4aaaee4c66ae94fb6c946bfe17700
Franck Delage <franck@web82.fr>
2026-04-27 08:16:42 +0200

    nvim: WIP - upgrade refactoring plugin - breaks when installing async dependency

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -9,38 +9,31 @@ local ts_js_print_var_statements = {
 return {
   'ThePrimeagen/refactoring.nvim',
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
   },
-  lazy = true,
-  event = 'BufReadPre',
+  lazy = false,
   config = function()
     require('refactoring').setup {
-      print_var_statements = {
-        typescript = ts_js_print_var_statements,
-        javascript = ts_js_print_var_statements,
-      },
     }
   end,
   keys = {
     -- Extract operations (visual mode)
     {
       '<leader>re',
-      function() return require('refactoring').refactor 'Extract Function' end,
-      mode = 'x',
+      function() return require('refactoring').extract_func() end,
+      mode = { 'x', 'n' },
       expr = true,
       desc = 'Extract function',
     },
     {
       '<leader>rf',
-      function() return require('refactoring').refactor 'Extract Function To File' end,
-      mode = 'x',
+      function() return require('refactoring').extract_func_to_file() end,
+      mode = { 'x', 'n' },
       expr = true,
       desc = 'Extract function to file',
     },
     {
       '<leader>rv',
-      function() return require('refactoring').refactor 'Extract Variable' end,
+      function() return require('refactoring').extract_var() end,
       mode = 'x',
       expr = true,
       desc = 'Extract variable',
@@ -48,56 +41,41 @@ return {
     -- Inline operations (normal mode)
     {
       '<leader>ri',
-      function() return require('refactoring').refactor 'Inline Function' end,
-      mode = 'n',
+      function() return require('refactoring').inline_func() end,
+      mode = { 'n', 'x' },
       expr = true,
       desc = 'Inline function',
     },
     {
       '<leader>rI',
-      function() return require('refactoring').refactor 'Inline Variable' end,
+      function() return require('refactoring').inline_var() end,
       mode = { 'n', 'x' },
       expr = true,
       desc = 'Inline variable',
     },
-    -- Block operations (normal mode)
-    {
-      '<leader>rb',
-      function() return require('refactoring').refactor 'Extract Block' end,
-      mode = 'n',
-      expr = true,
-      desc = 'Extract block',
-    },
-    {
-      '<leader>rB',
-      function() return require('refactoring').refactor 'Extract Block To File' end,
-      mode = 'n',
-      expr = true,
-      desc = 'Extract block to file',
-    },
     -- Debug statements
     {
       '<leader>rp',
-      function() require('refactoring').debug.print_var {} end,
+      function() return require('refactoring.debug').print_var { output_location = "below" } .. "iw" end,
       mode = { 'n', 'x' },
       desc = 'Debug: print variable',
     },
     {
       '<leader>rP',
-      function() require('refactoring').debug.printf { below = false } end,
+      function() return require('refactoring.debug').print_var { output_location = "above" } .. "iw" end,
       mode = 'n',
       desc = 'Debug: print function',
     },
     {
       '<leader>rc',
-      function() require('refactoring').debug.cleanup {} end,
+      function() return require('refactoring.debug').cleanup { restore_view = true } end,
       mode = 'n',
       desc = 'Debug: cleanup print statements',
     },
     -- Picker (all refactors via vim.ui.select)
     {
       '<leader>rr',
-      function() require('refactoring').select_refactor() end,
+      function() return require('refactoring').select_refactor() end,
       mode = { 'n', 'x' },
       desc = 'Select refactor',
     },
