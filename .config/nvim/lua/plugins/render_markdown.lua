7b467dfdb72ee7975a92d5eeb71df33f214c501b
Franck Delage <franck@web82.fr>
2026-05-12 16:51:20 +0200

    nvim: improve render-markdown config for octo buffers

--- .config/nvim/lua/plugins/render_markdown.lua
+++ .config/nvim/lua/plugins/render_markdown.lua
@@ -1,11 +1,20 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  init = function() vim.treesitter.language.register("markdown", "octo") end,
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
   opts = {
+    file_types = { "markdown", "octo" },
+    completions = {
+      blink = {
+        enabled = true,
+      },
+    },
     overrides = {
       buftype = {
         nofile = { enabled = false },
       },
     },
   },
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 }
