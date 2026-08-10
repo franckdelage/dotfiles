f4f7121a0fb91044de37daa966253194ea561c0e
Franck Delage <franck@web82.fr>
2026-08-10 16:54:57 +0200

    nvim: improve blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,17 +1,15 @@
 return {
-  'saghen/blink.cmp',
+  "saghen/blink.cmp",
   dependencies = {
-    'rafamadriz/friendly-snippets',
-    'mikavilpas/blink-ripgrep.nvim',
+    "rafamadriz/friendly-snippets",
+    "mikavilpas/blink-ripgrep.nvim",
   },
-  version = '*',
-  opts_extend = { 'sources.default' },
-  config = function(_, opts)
-    require('blink.cmp').setup(opts)
-  end,
+  version = "1.*",
+  opts_extend = { "sources.default" },
+  config = function(_, opts) require("blink.cmp").setup(opts) end,
   opts = {
     keymap = {
-      preset = 'default',
+      preset = "default",
       ["<Tab>"] = {
         "snippet_forward",
         function() -- sidekick next edit suggestion
@@ -22,53 +20,47 @@ return {
         end,
         "fallback",
       },
-      ['<Enter>'] = { 'accept', 'fallback' },
-      ['<C-e>'] = { 'cancel', 'fallback' },
-      ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-      ['<C-s>'] = {
-        function()
-          require('blink.cmp').show { providers = { 'snippets' } }
-        end,
+      ["<Enter>"] = { "accept", "fallback" },
+      ["<C-e>"] = { "cancel", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-s>"] = {
+        function() require("blink.cmp").show { providers = { "snippets" } } end,
       },
-      ['<C-l>'] = {
-        function()
-          require('blink.cmp').show { providers = { 'lsp' } }
-        end,
+      ["<C-l>"] = {
+        function() require("blink.cmp").show { providers = { "lsp" } } end,
       },
-      ['<c-t>'] = {
-        function()
-          require('blink.cmp').show { providers = { 'ripgrep' } }
-        end,
+      ["<c-t>"] = {
+        function() require("blink.cmp").show { providers = { "ripgrep" } } end,
       },
     },
     cmdline = {
+      keymap = { preset = "inherit" },
       enabled = true,
       completion = {
         menu = {
           auto_show = true,
         },
-        ghost_text = {
-          enabled = false,
-        },
       },
     },
     completion = {
       menu = {
-        auto_show = function(ctx)
-          return not vim.tbl_contains({ '/', '?' }, vim.fn.getcmdtype())
-        end,
-        border = 'rounded',
+        auto_show = function(ctx) return not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype()) end,
+        border = "rounded",
         draw = {
           padding = 2,
-          treesitter = { 'lsp' },
-          columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'source_name', gap = 2 } },
+          treesitter = { "lsp" },
+          columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name", gap = 2 } },
         },
       },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 250,
-        window = { border = 'rounded' },
+        window = { border = "rounded" },
       },
       list = {
         selection = {
@@ -79,33 +71,27 @@ return {
     },
     appearance = {
       use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono',
+      nerd_font_variant = "mono",
     },
     snippets = {
-      preset = 'luasnip',
-      expand = function(snippet)
-        require('luasnip').lsp_expand(snippet)
-      end,
+      preset = "luasnip",
+      expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
       active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
+        if filter and filter.direction then return require("luasnip").jumpable(filter.direction) end
+        return require("luasnip").in_snippet()
       end,
-      jump = function(direction)
-        require('luasnip').jump(direction)
-      end,
+      jump = function(direction) require("luasnip").jump(direction) end,
     },
     sources = {
-      default = { 'snippets', 'lsp', 'path', 'buffer' },
+      default = { "snippets", "lsp", "path", "buffer" },
       providers = {
         ripgrep = {
-          module = 'blink-ripgrep',
-          name = 'Ripgrep',
+          module = "blink-ripgrep",
+          name = "Ripgrep",
           ---@module "blink-ripgrep"
           ---@type blink-ripgrep.Options
           opts = {
-            project_root_marker = { '.git', 'nx.json' },
+            project_root_marker = { ".git", "nx.json" },
           },
         },
       },
@@ -113,15 +99,16 @@ return {
     signature = {
       enabled = false,
       window = {
-        border = 'rounded',
+        border = "rounded",
         show_documentation = false,
       },
     },
     fuzzy = {
+      implementation = "prefer_rust_with_warning",
       sorts = {
-        'score',
-        'kind',
-        'sort_text',
+        "score",
+        "kind",
+        "sort_text",
       },
     },
   },
