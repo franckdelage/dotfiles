d073d5dacdf42725258c86fb3e4e48c86cf1583a
Franck Delage <franck@web82.fr>
2026-08-21 13:48:37 +0200

    nvim: clean up gql config

--- .config/nvim/lua/lsp/servers/graphql.lua
+++ .config/nvim/lua/lsp/servers/graphql.lua
@@ -1,74 +1,37 @@
 local M = {}
 
--- GraphQL language server with Apollo Angular optimizations
+-- GraphQL language server, including gql templates embedded in JS/TS files.
 M.servers = {
   graphql = {
     cmd = { 'graphql-lsp', 'server', '-m', 'stream' },
     filetypes = { 'graphql', 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
     root_patterns = {
       '.graphqlconfig',
-      'apps/gql/.graphqlconfig',
-      'apollo.config.js',
-      'apollo.config.json',
-      '.graphqlrc*',
-      'graphql.config.*',
-      'codegen.yml',
-      'codegen.yaml',
+      '.graphqlrc',
+      '.graphqlrc.json',
+      '.graphqlrc.yaml',
+      '.graphqlrc.yml',
+      '.graphqlrc.js',
+      '.graphqlrc.ts',
+      'graphql.config.js',
+      'graphql.config.ts',
+      'graphql.config.json',
+      'graphql.config.yaml',
+      'graphql.config.yml',
       'nx.json',
       'package.json',
       '.git',
     },
     name = 'graphql',
-    settings = {
-      graphql = {
-        introspection = {
-          file = './apps/gql/schema.graphql',
-        },
-        completion = {
-          externalFragments = true,
-          addTypename = true,
-          useSchemaIntrospection = true,
-        },
-        validation = {
-          rules = {
-            'KnownTypeNames',
-            'UniqueFragmentNames',
-            'KnownFragmentNames',
-            'NoUnusedFragments',
-            'FieldsOnCorrectType',
-            'FragmentsOnCompositeTypes',
+    settings = function(root_dir)
+      return {
+        ['graphql-config'] = {
+          load = {
+            legacy = true,
+            rootDir = root_dir,
           },
         },
-        apollo = {
-          service = {
-            name = 'default',
-            localSchemaFile = './apps/gql/schema.graphql',
-          },
-        },
-      },
-    },
-    on_attach = function(client, bufnr)
-      local filetype = vim.bo[bufnr].filetype
-
-      if filetype == 'graphql' then
-        -- Full GraphQL features for .graphql files
-        return
-      end
-
-      -- For TypeScript files with Apollo
-      if filetype == 'typescript' or filetype == 'typescriptreact' then
-        -- Enable GraphQL features for gql template literals
-        client.server_capabilities.hoverProvider = true
-        client.server_capabilities.completionProvider = {
-          triggerCharacters = { '{', '(', '$', '@', '.', ' ', 'g', 'q', 'l' },
-          resolveProvider = true,
-        }
-        client.server_capabilities.documentFormattingProvider = false -- Let Prettier handle this
-
-        -- Keep definition/references disabled to avoid conflicts with tsserver
-        client.server_capabilities.definitionProvider = false
-        client.server_capabilities.referencesProvider = false
-      end
+      }
     end,
   },
 }
