e80a5c70e61c7f7a01f397a1b0df0bc56d8f3974
Franck Delage <franck@web82.fr>
2026-09-07 11:41:58 +0200

    zsh: change bw aliases from yarn to npm

--- .zsh/work.zsh
+++ .zsh/work.zsh
@@ -5,30 +5,24 @@ export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-alias smokerecord='clear && yarn nx run e2e:record'
-alias smokereplay='clear && yarn nx run e2e:replay'
-alias smokeheadless='clear && yarn nx run e2e:headless'
-alias fakesession='clear && yarn nx run fake-session:serve'
-
-alias nxg='yarn nx g'
-alias nxgm='yarn nx g module'
-alias nxgc='yarn nx g @ngneat/spectator:spectator-component'
-alias nxgs='yarn nx g @ngneat/spectator:spectator-service'
-alias nxgd='yarn nx g @ngneat/spectator:spectator-directive'
+alias nxg='npm run nx -- g'
+alias nxgm='npm run nx -- g module'
+alias nxgc='npm run nx -- g @ngneat/spectator:spectator-component'
+alias nxgs='npm run nx -- g @ngneat/spectator:spectator-service'
+alias nxgd='npm run nx -- g @ngneat/spectator:spectator-directive'
 
 alias apirequests="cd ~/Developer && mkdir -p mitm-files && cd mitm-files && mitmproxy --listen-port=8080 --set view_filter='!beacon & !pharos' --set console_focus_follow=true --set console_default_contentview='json' --mode upstream:http://127.0.0.1:8081 --ssl-insecure"
 
-alias appstart='yarn nx run touchpoint-web:serve:ute3'
-alias gqlstart='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost yarn nx run gql:serve:ute3'
-alias gqlstartd='yarn nx run gql:serve:ute3'
-alias serverstart='yarn nx run touchpoint-web:server:ute3'
-alias exchangestart='yarn nx run exchange:serve:ute3'
+alias appstart='npm run nx -- run touchpoint-web:serve:ute3'
+alias gqlstart='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost npm run nx -- run gql:serve:ute3'
+alias gqlstartd='npm run nx -- run gql:serve:ute3'
+alias exchangestart='npm run nx -- run exchange:serve:ute3'
 
-alias gqlstartute2='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost yarn nx run gql:serve:ute2'
-alias serverstartute2='yarn nx run touchpoint-web:server:ute2'
+alias gqlstartute2='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost npm run nx -- run gql:serve:ute2'
+alias serverstartute2='npm run nx -- run touchpoint-web:server:ute2'
 
-alias introspect='(cd apps/gql && yarn graphql:introspect)'
+alias introspect='(cd apps/gql && npm run graphql:introspect)'
 
-alias nxr='clear && yarn nx run'
+alias nxr='clear && npm run nx -- run'
 
 alias mocksengine="mkdir -p ~/Developer/mitm/mocked-responses && mkdir -p ~/Developer/mitm/scripts && touch ~/Developer/mitm/scripts/save_responses.py && mitmdump -s ~/Developer/mitm/scripts/save_responses.py --listen-port 8081"
