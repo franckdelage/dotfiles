b806687b2ba9c72d1b8d8c63f3383ae77eb8535c
Franck Delage <franck@web82.fr>
2026-09-11 16:39:25 +0200

    nvim: patch neogit while codediff integration is not fixed

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -16,6 +16,25 @@ return {
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
   },
   config = function()
+    -- Compatibility shim for Neogit's legacy CodeDiff explorer schema.
+    -- Remove when https://github.com/NeogitOrg/neogit/issues/2008 is fixed.
+    local codediff_view = require "codediff.ui.view"
+    local codediff_path = require "codediff.core.path"
+    local original_create = codediff_view.create
+
+    codediff_view.create = function(session_config, filetype, on_ready)
+      if session_config.mode == "explorer" and not session_config.panel then
+        session_config.panel = {
+          name = "explorer",
+          data = session_config.explorer_data or {},
+        }
+        session_config.original = session_config.original or codediff_path.empty()
+        session_config.modified = session_config.modified or codediff_path.empty()
+      end
+
+      return original_create(session_config, filetype, on_ready)
+    end
+
     require("neogit").setup {
       integrations = {
         codediff = true,
