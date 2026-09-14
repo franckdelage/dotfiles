0e1f80c6a6e809ce1b3068dedd07f9a92bf931b5
Franck Delage <franck@web82.fr>
2026-09-14 10:51:57 +0200

    zsh: ute2 aliases and start them in sesh

--- .zsh/work.zsh
+++ .zsh/work.zsh
@@ -13,13 +13,15 @@ alias nxgd='npm run nx -- g @ngneat/spectator:spectator-directive'
 
 alias apirequests="cd ~/Developer && mkdir -p mitm-files && cd mitm-files && mitmproxy --listen-port=8080 --set view_filter='!beacon & !pharos' --set console_focus_follow=true --set console_default_contentview='json' --mode upstream:http://127.0.0.1:8081 --ssl-insecure"
 
+# UTE3
 alias appstart='npm run nx -- run touchpoint-web:serve:ute3'
 alias gqlstart='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost npm run nx -- run gql:serve:ute3'
-alias gqlstartd='npm run nx -- run gql:serve:ute3'
 alias exchangestart='npm run nx -- run exchange:serve:ute3'
 
+# UTE2
+alias appstartute2='npm run nx -- run touchpoint-web:serve:ute2'
 alias gqlstartute2='HTTPS_PROXY=http://localhost:8080 NODE_TLS_REJECT_UNAUTHORIZED=0 FEATURE_ENV=localhost npm run nx -- run gql:serve:ute2'
-alias serverstartute2='npm run nx -- run touchpoint-web:server:ute2'
+alias exchangestartute2='npm run nx -- run exchange:serve:ute2'
 
 alias introspect='(cd apps/gql && npm run graphql:introspect)'
 
